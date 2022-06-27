fdbfb2237403b1d9bdbbf896b4f4fed8a227fe3e
Manuel Eberhardinger <eberhardinger@hdm-stuttgart.de>
2022-06-27 11:27:17 +0200

    added script to test the environemnts fast

---
+++ run_marl_experiments.sh
@@ -0,0 +1,7 @@
+python src/main.py --config=iql --env-config=gymma with checkpoint_path=$1 evaluate=True  env_args.key="lbforaging:Foraging-grid-5x5-2p-2f-v2"
+python src/main.py --config=iql --env-config=gymma with checkpoint_path=$1 evaluate=True  env_args.key="lbforaging:Foraging-grid-5s-8x8-2p-4f-v2"
+python src/main.py --config=iql --env-config=gymma with checkpoint_path=$1 evaluate=True  env_args.key="lbforaging:Foraging-grid-5s-10x10-2p-4f-v2"
+python src/main.py --config=iql --env-config=gymma with checkpoint_path=$1 evaluate=True  env_args.key="lbforaging:Foraging-grid-5s-12x12-2p-4f-v2"
+python src/main.py --config=iql --env-config=gymma with checkpoint_path=$1 evaluate=True  env_args.key="lbforaging:Foraging-grid-5s-14x14-2p-5f-v2"
+python src/main.py --config=iql --env-config=gymma with checkpoint_path=$1 evaluate=True  env_args.key="lbforaging:Foraging-grid-5s-16x16-2p-6f-v2"
+python src/main.py --config=iql --env-config=gymma with checkpoint_path=$1 evaluate=True  env_args.key="lbforaging:Foraging-grid-5s-18x18-2p-8f-v2"
